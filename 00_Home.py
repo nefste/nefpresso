3969ad859b05eb201f8af05617d61efbb4e144ed
Stephan Nef <65945515+nefste@users.noreply.github.com>
2023-11-16 16:35:57 +0100

    fancy

--- 00_Home.py
+++ 00_Home.py
@@ -1,10 +1,49 @@
 import streamlit as st
-import hmac
+
 
 st.set_page_config(
     page_title="Hello",
-    page_icon="👋",
+    page_icon="👨🏻‍💻",
 )
 
-st.title('Hello World')
-st.subheader('☕Welcome at Nefpresso, please login to continue...')
+
+streamlit_style = """
+			<style>
+			@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
+
+			html, body, [class*="css"]  {
+			font-family: 'Roboto';
+			}
+			</style>
+			"""
+st.markdown(streamlit_style, unsafe_allow_html=True)
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+
+st.title('Nefpresso')
+
+st.markdown('<p class="css">Hello World und so...</p>', unsafe_allow_html=True)
+
+
+## LOGIN just for fun:
+# dial = st.dialog("Login", close_on_submit=False)
+# with dial:
+#     name = st.text_input("Enter Name")
+#     password = st.text_input("Enter Password")
+#     if st.form_submit_button("Submit"):
+#         if name != "Stephan" :
+#             st.error("Sorry, heute nicht...")
+#         else:
+#             dial.close()
+# name = None
+
+# if name is None :
+#     dial.open()
+    
--- pages/01_About_Me.py
+++ pages/01_About_Me.py
@@ -1,9 +1,57 @@
 import streamlit as st
+from streamlit_ace import st_ace
 
 st.set_page_config(
     page_title="About Me",
-    page_icon="👋",
+    page_icon="👨🏻‍💻",
 )
 
-st.title('About Me')
-st.subheader('My name is Stephan Nef')
+
+streamlit_style = """
+			<style>
+			@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
+
+			html, body, [class*="css"]  {
+			font-family: 'Roboto';
+			}
+			</style>
+			"""
+st.markdown(streamlit_style, unsafe_allow_html=True)
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+
+st.title("Stephan Nef")
+
+st.markdown('<p class="css">Da kommt noch was...</p>', unsafe_allow_html=True)
+
+
+
+
+code = st_ace(
+    value="""print("Hello at Nefpresso!")""",
+    language='python', 
+    theme='tomorrow_night',
+    tab_size= 4,
+    font_size=16, height=120
+)
+
+html = f"""
+<html>
+  <head>
+    <link rel="stylesheet" href="https://pyscript.net/latest/pyscript.css" />
+    <script defer src="https://pyscript.net/latest/pyscript.js"></script>
+  </head>
+  <body>
+    <py-script>{code}</py-script>
+  </body>
+</html>
+"""
+# st.markdown('<p class="css">Output:</p>', unsafe_allow_html=True)
+st.components.v1.html(html, height=200, scrolling=False)
